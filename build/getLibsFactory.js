113fd8cbeaff9132b5a473beb52d5e9b4c650c9d
PUGE <mail@puge.org>
2024-07-04 05:32:02 +1000

    fix bug

--- build/getLibsFactory.js
+++ build/getLibsFactory.js
@@ -10,7 +10,7 @@ function getLibsFactory(args) {
         return isWin() ? 'lib' : (isOSX() ? 'dylib' : 'so');
     }
     function getLibNameRegex(opencvModuleName) {
-        return new RegExp("^" + getLibPrefix() + opencvModuleName + "[0-9]{0,3}." + getLibSuffix() + "$");
+        return new RegExp("^" + getLibPrefix() + opencvModuleName + "[0-9]{0,4}." + getLibSuffix() + "$");
     }
     function createLibResolver(libDir) {
         function getLibAbsPath(libFile) {
